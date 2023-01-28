9558d06f3c90a9bccfc79ecf3bde9996f41b4bf5
Lucas Fontenele <lucasfontenele@MacBook-Pro-de-Lucas.local>
2023-01-28 01:56:07 -0300

    verificando menor distancia

--- js/main.js
+++ js/main.js
@@ -21,18 +21,6 @@ function init(params) {
   addObjects();
 }
 
-let data = new Uint8Array([
-  255, 0, 0, 255,   // Red pixel at (0, 0)
-  0, 255, 0, 255,   // Green pixel at (1, 0)
-  0, 0, 255, 255,   // Blue pixel at (2, 0)
-  255, 0, 0, 255,   // Red pixel at (0, 1)
-  0, 0, 0, 255,     // Black pixel at (1, 1)
-  0, 0, 255, 255,   // Blue pixel at (2, 1)
-  255, 0, 0, 255,   // Red pixel at (0, 2)
-  0, 0, 0, 255,     // Black pixel at (1, 2)
-  0, 0, 255, 255    // Blue pixel at (2, 2)
-]);
-
 // Loop para percorrer o viewport
 function loop() {
   var pixel = new THREE.Vector2();
@@ -43,25 +31,13 @@ function loop() {
 
       pixel.x = (i / canvas.clientWidth) * 2 - 1;
       pixel.y = -(j / canvas.clientHeight) * 2 + 1;
+
       // Verifica a posição do raio baseado nas coordenadas do pixel a partir da câmera
       raycaster.setFromCamera(pixel, camera);
 
-      // let texture = material.map;
-      // let pixelBuffer = new Uint8Array(4); // 4 for RGBA
-      // texture.image.data = pixelBuffer;
-    
-      // // Change the color of the pixel
-      // texture.getPixel(pixel.x, pixel.y, pixelBuffer);
-      // pixelBuffer[0] = 255; // Red
-      // pixelBuffer[1] = 0; // Green
-      // pixelBuffer[2] = 0; // Blue
-      // pixelBuffer[3] = 255; // Alpha
-      // texture.setPixel(pixel.x, pixel.y, pixelBuffer);
-      // texture.needsUpdate = true;
-
       // Verifica intersecao
       intersects = raycaster.intersectObjects(scene.children, true);
-      console.log(intersects);
+      // console.log(intersects);
 
       for (var k = 0; k < intersects.length; k++) {
         intersects[k].object.material.color.set(0x000FFD);
@@ -72,8 +48,6 @@ function loop() {
     }
   }
   console.log("Done");
-  
-  // requestAnimationFrame(loop);
 }
 
 function render(params) {
