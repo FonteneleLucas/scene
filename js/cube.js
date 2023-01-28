ae62976065d19ed72aef119b9bea2fcd493aa66f
Lucas Fontenele <lucasfontenele@MacBook-Pro-de-Lucas.local>
2023-01-28 00:00:24 -0300

    refatorando

--- js/cube.js
+++ js/cube.js
@@ -1,15 +1,14 @@
 
 
 function getCube(params) {
-    // Add objects to the scene
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshStandardMaterial({color: 0xff0000});
 
-
     const cube = new THREE.Mesh(geometry, material);
-    // cube.position.x = 2
+
     cube.position.y = 2
     cube.position.x = 2
+    cube.position.z = 1
     return cube;
 }
 
